82210304ebba88127ad72e2eb57a441b43185b69
Darshan Patel <darshanpatel72247@gmail.com>
2023-08-23 12:23:24 -0400

    UI App Design Completed

--- RockPaperScissors/ViewController.swift
+++ RockPaperScissors/ViewController.swift
@@ -9,6 +9,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //Winner Label #Winner:Computer
+    @IBOutlet weak var winnerLabel: UILabel!
+    
+    //Player and Computer Optined Picked Labels
+    @IBOutlet weak var playerPickLabel: UILabel!
+    @IBOutlet weak var computerPickLabel: UILabel!
+    
+    //Player and Computer Score Labels
+    @IBOutlet weak var playerScoreLabel: UILabel!
+    @IBOutlet weak var computerScoreLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
